0e532f56591ab503bfaf5ce1a42d581f292a28bb
Alex Oliver <emmet2by4@gmail.com>
2025-10-08 07:34:30 +0100

    Add files via upload

---
+++ version-3/server.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+clients = {}
+gameState = False
+
+@socketio.on('message')
+def handle_message(msg):
+    if msg[0] == "join":
+        clients[request.sid] = msg[1]
+        send(["join", msg[1]], broadcast=True)
+        print(f"join: {msg[1]}")
+    elif msg == "game":
+        send(["game", gameState], broadcast=True)
+        print(f"player has requested game statem which is {gameState}.")
+    else:
+        send(msg, broadcast=True)
+        print(f"received: {msg}")
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    name = clients.pop(request.sid, None)
+    if name:
+        send(["leave", name], broadcast=True)
+        print(f"leave: {name}")
+
+socketio.run(app, host='0.0.0.0', port=5000)
