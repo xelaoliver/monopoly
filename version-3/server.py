d744661a97f6a2feda2018c11181a6f9e0cfb440
Alex Oliver <emmet2by4@gmail.com>
2025-10-09 17:38:31 +0100

    Update server.py
    cool shi

--- version-3/server.py
+++ version-3/server.py
@@ -9,26 +9,39 @@ gameState = False
 
 @socketio.on('message')
 def handle_message(msg):
+    print(f"received: {msg}")
+
     if msg[0] == "join":
         clients[request.sid] = msg[1]
-        clientsList = []
-        for i in range(len(clients)):
-            clientsList.append(clients[list(clients.keys())[i]])
+        
+        clientsList = list(clients.values())
+        
         send(["join", msg[1], clientsList], broadcast=True)
         print(f"join: {msg[1]}")
-        print(clients)
+        print("clients:", clients, "client names:", clientsList)
     elif msg == "game":
-        send(["game", gameState], broadcast=True)
+        send(["game", gameState, ], broadcast=True)
         print(f"player has requested game statem which is {gameState}.")
+    elif msg[0] == "newclient":
+        clientsList = list(clients.values())
+
+        if msg[1] in clientsList:
+            send(["newclientreturn", False], to=request.sid)
+            print("client attempted to put in already existing name:", msg[1])
+        else:
+            clients[request.sid] = msg[1]
+            send(["newclientreturn", True], to=request.sid)
+            print("client put in new name:", msg[1])
     else:
         send(msg, broadcast=True)
-        print(f"received: {msg}")
 
 @socketio.on('disconnect')
 def handle_disconnect():
     name = clients.pop(request.sid, None)
     if name:
-        send(["leave", name], broadcast=True)
+        clientsList = list(clients.values())
+
+        send(["leave", name, clientsList], broadcast=True)
         print(f"leave: {name}")
 
 socketio.run(app, host='0.0.0.0', port=5000)
