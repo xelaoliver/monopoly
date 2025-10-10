5007a2f47e7131e3ebc408d3a06f3c5de7aeae02
Alex Oliver <emmet2by4@gmail.com>
2025-10-10 18:31:19 +0100

    Update client.js

--- version-3/client.js
+++ version-3/client.js
@@ -1,6 +1,7 @@
 const socket = io("http://localhost:5000");
 offonline = true;
 nameVerify = false;
+oldVoteKickName = "";
 
 socket.on("connect", () => {
     console.log("yay. server is connected to client!");
@@ -8,7 +9,9 @@ socket.on("connect", () => {
     if (((window.innerWidth <= 800) && (window.innerHeight <= 600))) {
         document.getElementById("offonline").innerHTML = 'This site requires you to have a keyboard/mouse and a display larger than 800x600px.';
     } else {
-        document.getElementById("offonline").innerHTML = 'Name: <input type="text" id="name" autocomplete="off"> <button onclick="join()">Join</button><br><br>The server is <span style="color: green;">online</span>';
+        document.getElementById("offonline").innerHTML = `
+        Name: <input type="text" id="name" autocomplete="off"> <button onclick="join()">
+        Join</button><br><br>The server is <span style="color: green;">online</span>`;
         socket.send("game");
     }
 });
@@ -22,7 +25,9 @@ socket.on("message", (msg) => {
 
     if (msg[0] == "game" && offonline) {
         if (msg[1]) {
-            document.getElementById("offonline").innerHTML = 'The server is <span style="color: green;">online</span>, but a game is already in play.<br>Please wait until the game ends.';
+            document.getElementById("offonline").innerHTML = `
+            The server is <span style="color: green;">online</span>
+            , but a game is already in play.<br>Please wait until the game ends.`;
             console.log("game already in play");
         } else {
             document.getElementById("offonline").innerHTML += ' and players will be waiting in the lobby.';
@@ -45,6 +50,16 @@ socket.on("message", (msg) => {
             
             document.getElementById("player-list").appendChild(client);
         }
+
+        document.getElementById("vote-kick").textContent = "";
+        for (let i = 0; i < msg[2].length; i++) {
+            const client = document.createElement("option");
+            client.textContent = msg[2][i];
+            client.id = msg[2][i];
+            client.value = msg[2][i];
+            
+            document.getElementById("vote-kick").appendChild(client);
+        }
     } else if (msg[0] == "join") {
         document.getElementById("player-number").innerHTML = msg[2].length;
 
@@ -57,6 +72,16 @@ socket.on("message", (msg) => {
             document.getElementById("player-list").appendChild(client);
         }
 
+        document.getElementById("vote-kick").textContent = "";
+        for (let i = 0; i < msg[2].length; i++) {
+            const client = document.createElement("option");
+            client.textContent = msg[2][i];
+            client.id = msg[2][i];
+            client.value = msg[2][i];
+            
+            document.getElementById("vote-kick").appendChild(client);
+        }
+
         document.getElementById("log").innerHTML = `<span style="color: red;">${msg[1]} has joined the game.</span><br>`+document.getElementById("log").innerHTML;
     } else if (msg[0] == "newclientreturn") {
         nameVerify = msg[1];
@@ -67,12 +92,27 @@ socket.on("message", (msg) => {
 
             document.getElementById("home").style.display = "none";
             document.getElementById("game").style.display = "grid";
+
+            document.getElementById("name").value = "";
         } else {
             console.log("not good name");
 
             alert("This name has been taken by "+name+", please use another one.");
         }
-        }
+    } else if (msg == "kick") {
+        console.log("you have been kicked, lol");
+
+        document.getElementById("home").style.display = "inline";
+        document.getElementById("game").style.display = "none";
+    } else if (msg[0] == "votekick") {
+        document.getElementById("log").innerHTML = `<span style="color: red;">${msg[2]} has cast a vote to kick ${msg[1]}.</span><br>`+document.getElementById("log").innerHTML;
+    } else if (msg[0] == "votestart") {
+        document.getElementById("log").innerHTML = `<span style="color: red;">${msg[1]} has cast a vote to start the game.</span><br>`+document.getElementById("log").innerHTML;
+    } else if (msg[0] == "voteholdstart") {
+        document.getElementById("log").innerHTML = `<span style="color: red;">${msg[1]} has cast a vote to hold the start of the game.</span><br>`+document.getElementById("log").innerHTML;
+    } else if (msg == "gamestart") {
+        console.log("!!!!!game has started!!!!!");
+    }
 
     document.getElementById("log").scrollTop = 0;
 });
@@ -96,4 +136,36 @@ function sendMessage() {
 
     socket.send(["msg", name, msg]);
     document.getElementById("message").value = "";
+}
+
+function voteStart(start) {
+    if (start) {
+        socket.send(["votestart", name]);
+
+        document.getElementById("vote-start").style.display = "none";
+        document.getElementById("vote-hold-start").style.display = "inline";
+
+        console.log("voted to start");
+    } else {
+        socket.send(["voteholdstart", name]);
+
+        document.getElementById("vote-start").style.display = "inline";
+        document.getElementById("vote-hold-start").style.display = "none";
+
+        console.log("voted to hold start");
+    }
+}
+
+function voteKick() {
+    var voteKickName = document.getElementById("vote-kick").value;
+
+    if (voteKickName != oldVoteKickName) {
+        socket.send(["votekick", voteKickName, name]);
+
+        console.log("voted to kick "+voteKickName);
+    } else {
+        console.log("already voted to kick "+voteKickName);
+    }
+
+    oldVoteKickName = voteKickName;
 }
