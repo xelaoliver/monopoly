70e60a12cce7f86d0aa15da645dd71e10648fe7a
Alex Oliver <emmet2by4@gmail.com>
2025-10-11 17:58:55 +0100

    Add files via upload

---
+++ version-3/board.js
@@ -0,0 +1,27 @@
+const container = document.getElementById('canvas-container');
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  container.clientWidth / (container.clientHeight - 16),
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( container.clientWidth, container.clientHeight-16 );
+container.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
+}
+renderer.setAnimationLoop( animate );
